b6a9a5a10fe6125495b330a1731cd5c9ffbce154
DimitarGenchev <d.genchev21@gmail.com>
2024-02-18 22:09:09 +0200

    Added Category, Brand and Product models

--- GymMembershipsApp/gym/admin.py
+++ GymMembershipsApp/gym/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from GymMembershipsApp.gym.models import Membership
+from GymMembershipsApp.gym.models import Membership, Category, Product, Brand
 
 
 @admin.register(Membership)
 class MembershipAdmin(admin.ModelAdmin):
     fields = ['duration', 'membership_type', 'user', 'end_date']
     readonly_fields = ['end_date']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    ...
--- GymMembershipsApp/gym/models.py
+++ GymMembershipsApp/gym/models.py
@@ -29,10 +29,10 @@ class Membership(models.Model):
             return max(len(choice.value) for choice in cls)
 
     class DurationChoices(models.IntegerChoices):
-        ONE_MONTH = 1, "1 month"
-        THREE_MONTHS = 3, "3 months"
-        SIX_MONTHS = 6, "6 months"
-        TWELVE_MONTHS = 12, "12 months"
+        ONE_MONTH = 1, '1 month'
+        THREE_MONTHS = 3, '3 months'
+        SIX_MONTHS = 6, '6 months'
+        TWELVE_MONTHS = 12, '12 months'
 
     duration = models.IntegerField(
         choices=DurationChoices.choices,
@@ -73,3 +73,54 @@ class Membership(models.Model):
             self.save()
 
         return result
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=30,
+    )
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(
+        max_length=30,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=50,
+    )
+
+    description = models.TextField(
+        null=True,
+        blank=True,
+    )
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+    )
+
+    brand = models.ForeignKey(
+        Brand,
+        on_delete=models.CASCADE,
+    )
+
+    price = models.DecimalField(
+        max_digits=4,
+        decimal_places=2,
+    )
+
+    def __str__(self):
+        return f'{self.name} - {self.brand}'
+
