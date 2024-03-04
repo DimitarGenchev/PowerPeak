6d76819ee2afd6ee36266edd7bc969d95080474a
DimitarGenchev <d.genchev21@gmail.com>
2024-03-04 21:27:14 +0200

    Added sections our-story and our-mission with responsive styling

--- GymMembershipsApp/gym/forms.py
+++ GymMembershipsApp/gym/forms.py
@@ -6,4 +6,10 @@ UserModel = get_user_model()
 class UserRegisterForm(auth_forms.UserCreationForm):
     class Meta(auth_forms.UserCreationForm.Meta):
         model = UserModel
-        fields = ['email', 'password1', 'password2']
+        fields = [
+            'email',
+            'password1',
+            'password2',
+            'first_name',
+            'last_name',
+        ]
