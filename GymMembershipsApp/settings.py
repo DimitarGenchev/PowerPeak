49614f51aac37139a9a0826bf17d1404ce727b0d
DimitarGenchev <d.genchev21@gmail.com>
2024-12-08 23:46:26 +0200

    Fixed CSRF issue

--- GymMembershipsApp/settings.py
+++ GymMembershipsApp/settings.py
@@ -19,6 +19,7 @@ DEBUG = os.getenv('DEBUG', config('DEBUG', default=False)) == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', config('ALLOWED_HOSTS', default=[])).split(',')
 
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', default=[])
 
 PROJECT_APPS = [
     'GymMembershipsApp.gym',
