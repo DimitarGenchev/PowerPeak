d5cdbe57fcfeb6d71157fa94ee5988fe29f1c1b0
DimitarGenchev <d.genchev21@gmail.com>
2024-12-10 23:24:04 +0200

    Fixed media files not being served

--- GymMembershipsApp/urls.py
+++ GymMembershipsApp/urls.py
@@ -1,12 +1,17 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.i18n import i18n_patterns
+from django.views.static import serve
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('GymMembershipsApp.gym.urls')),
     path('users/', include('GymMembershipsApp.users.urls')),
     prefix_default_language=False
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
